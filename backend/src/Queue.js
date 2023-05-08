9fe537f99964c9b91bc14cf887712af3141707dd
gabriel@desktop <gabrieljoserporto@gmail.com>
2023-05-08 10:18:21 -0300

    create: Iniciando o front

--- backend/src/Queue.js
+++ backend/src/Queue.js
@@ -4,17 +4,20 @@ class Queue {
     this.frontIndex = 0;
     this.backIndex = 0;
   }
+
   push(item) {
     this.items.push(item);
     this.backIndex++;
     return item + " inserido na posição " + this.backIndex;
   }
+
   shift() {
     const item = this.items[this.frontIndex];
     this.items.shift();
     this.frontIndex++;
     return item + " retirado ";
   }
+
   peek() {
     return this.items[this.frontIndex];
   }
@@ -22,9 +25,15 @@ class Queue {
   emailAlreadyQueued(item) {
     return this.items.some((email) => email === item);
   }
+
+  findPositionByEmail(item) {
+    return this.items.findIndex((email) => email === item) + 1
+  }
+
   get printQueue() {
     return this.items;
   }
+  
   get length() {
     return this.items.length;
   }
